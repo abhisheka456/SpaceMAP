4eaf9270d6e4de26f32b28651f701ba7e248923f
Abhishek Kumar <47485978+abhisheka456@users.noreply.github.com>
2022-11-03 18:20:19 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='SpaceMAP',
+    version='0.0.1',
+    author='',
+    author_email='',
+    description='',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='',
+    project_urls = {
+        "Bug Tracker": ""
+    },
+    license='MIT',
+    packages=['SpaceMAP'],
+    install_requires=['requests'],
+)
